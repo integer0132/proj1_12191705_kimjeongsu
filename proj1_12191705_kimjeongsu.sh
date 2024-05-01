aed2ae5531c6d2905cf1e16ba255a3bf5116be06
integer0132 <integer0132@gmail.com>
2024-05-01 06:29:22 +0000

    add whoami

--- proj1_12191705_kimjeongsu.sh
+++ proj1_12191705_kimjeongsu.sh
@@ -4,4 +4,10 @@ if [ $# -ne 3 ]
 then
 	echo "usage: $0 file1 file2 file3"
 	echo ""
+	exit 1
 fi
+
+echo "************OSS1 - Project1************"
+echo "*	StudentID : 12191705	      *"
+echo "*	Name : Jeongsu Kim	      *"
+echo "***************************************"
