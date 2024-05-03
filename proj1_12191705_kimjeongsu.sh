a7ac596f1abbc6785c42151fa8da65c2c2525f69
integer0132 <integer0132@gmail.com>
2024-05-03 02:32:47 +0000

    Finish OSS Project1

--- proj1_12191705_kimjeongsu.sh
+++ proj1_12191705_kimjeongsu.sh
@@ -115,14 +115,14 @@ do
 		fi;;
 	6)
 		n6=1
-######
+
 		while [ "$n6" -le 10 ]
 		do
 			cat ./$1 | awk -F, -v temp_n6="$n6" 'NR==temp_n6{printf("%d) %-25s\t", temp_n6, $1)}'
 			cat ./$1 | awk -F, -v temp_n6="$n6" 'NR==temp_n6+10{printf("%d) %-25s\n", temp_n6+10, $1)}'
 			n6=$(( n6+1 ))
 		done
-######
+
 		read -p "Enter your team number : " func6_num
 		
 		if [ -n "$func6_num" ] && [ "$func6_num" -ge 1 ]
